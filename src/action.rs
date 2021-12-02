819ab52832d15aa38d937994f9ea06eb309d8bca
Zura Benashvili <zura.benashvili@gmail.com>
2021-12-02 18:58:00 +0400

    feat(action): default implementation for `EnablingCondition`

--- src/action.rs
+++ src/action.rs
@@ -79,5 +79,7 @@ pub trait EnablingCondition<State> {
     /// Enabling condition for the Action.
     ///
     /// Checks if the given action is enabled for a given state.
-    fn is_enabled(&self, state: &State) -> bool;
+    fn is_enabled(&self, #[allow(unused_variables)] state: &State) -> bool {
+        true
+    }
 }
